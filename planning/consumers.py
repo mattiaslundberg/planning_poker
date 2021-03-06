9d11ff8a175fcef904ae5a59d1f9a60abd5cd4d1
Mattias Lundberg <me@mlundberg.se>
2021-03-06 08:28:22 +0100

    Async PlanningConsumer

--- planning/consumers.py
+++ planning/consumers.py
@@ -1,17 +1,28 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class PlanningConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+class PlanningConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.topic_name = self.scope["url_route"]["kwargs"]["topic_name"]
+        self.topic_group_name = "planning_%s" % self.topic_name
 
-    def disconnect(self, close_code):
-        pass
+        await self.channel_layer.group_add(self.topic_group_name, self.channel_name)
 
-    def receive(self, text_data):
+        await self.accept()
+
+    async def disconnect(self, _):
+        await self.channel_layer.group_discard(self.room_topic_name, self.channel_name)
+
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
-        print(text_data)
 
-        self.send(text_data=json.dumps({"message": message}))
+        await self.channel_layer.group_send(
+            self.topic_group_name, {"type": "vote_message", "message": message}
+        )
+
+    async def vote_message(self, event):
+        message = event["message"]
+
+        await self.send(text_data=json.dumps({"message": message}))
