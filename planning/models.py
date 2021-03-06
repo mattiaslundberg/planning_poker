9020539e8a23a99ca0d87d6e5146ec0032fa798e
Mattias Lundberg <me@mlundberg.se>
2021-03-06 12:03:24 +0100

    Properly index votes

--- planning/models.py
+++ planning/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 
 class Votes(models.Model):
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["session_id", "client_id"], name="unique_vote_per_user"
+            )
+        ]
+
     session_id = models.CharField(max_length=192)
     client_id = models.CharField(max_length=192)
     value = models.CharField(max_length=32)
